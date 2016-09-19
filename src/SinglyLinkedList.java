08732bc6dbce702f310f27c5cb239c93856ad5b4
Somanat S <ssom.ts@gmail.com>
2016-09-19 18:01:21 +0700

    HiHay

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -20,74 +20,109 @@ public class SinglyLinkedList {
     
     public void popBack() {
         if (isEmpty()){
- 
+            System.out.print("ERROR");
         }else{
-
+            Node temp=head;
+            while (temp.next.next != null){
+                temp=temp.next;
+            }
+            temp.next = null;
         }
     }
     
     public void popFront(){
         if (isEmpty()){
- 
+            System.out.print("ERROR");
         }else{
-
+            head=head.next;
         }
     }
     
     public Node topFront(){
         if (isEmpty()){
-            return new Node();
+            System.out.print("ERROR");
         } else {
-            return new Node();
+            return head;
         }
     }
     
     public Node topBack(){ 
         if (isEmpty()){
-            return new Node();
+            System.out.print("ERROR");
         } else {
-            return new Node();
+            Node temp=head;
+            while (temp.next.next != null){
+                temp=temp.next;
+            }
+            return temp.next;
         }
     }
     
     public void pushFront(Node node){
+        node.next=null;
         if (isEmpty()){
-
+            head.next=node;
         }else{
-
+            node.next=head.next.next;
+            head.next=node;
         }
     }
     
     public void pushBack(Node node) {
+        node.next=null;
         if (isEmpty()){
-
+            head.next=node;
         } else {
-
+            Node temp=head;
+            while (temp.next.next != null){
+                temp=temp.next;
+            }
+            temp.next=node;
         }
     }
 
     public Node findNode(int id){
         if (isEmpty()){
-            return new Node();
+            return new Node(0,"EMPTY LIST",0);
         } else {
-            return new Node();
+            Node temp=head;
+            while (temp.next.next != null){
+                temp=temp.next;
+                if(temp.student_id=id){
+                    return id;
+                }
+            }
         }
+        return new Node(0,"ID NOT FOUND",0);
     }
     
     public Node eraseNode(int id){
         if (isEmpty()){
-            return new Node();
+            return new Node(0,"EMPTY LIST",0);
         } else {
-            return new Node();
+            Node temp=head;
+            while (temp.next.next != null){
+                temp=temp.next;
+                if(temp.student_id=id){
+                    break;
+                }
+            }
+
         }
     }
     
     public void addNodeAfter(Node node1, Node node2){
-
+        node2.next=node1.next;
+        node1.next=node2;
     }
     
     public void addNodeBefore(Node node1, Node node2){
-
+        Node temp=head;
+        while (temp.next.next != node1){
+            temp=temp.next;
+        }
+        node2.next=node1;
+        temp.next=node2;
     }
     
     public boolean isEmpty(){
@@ -98,7 +133,11 @@ public class SinglyLinkedList {
     }
     
     public void printStructure(){
-        System.out.println("Hello World!");
+        Node temp=head;
+        while (temp.next.next != null){
+            temp=temp.next;
+            System.out.println(temp.name);
+        }
     }
     
     public Node whoGotHighestGPA(){
